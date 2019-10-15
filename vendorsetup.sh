cb07a68be352881660c83c4c97afdcf53a4a92ce
buffal0b1ll <billblandford@gmail.com>
2019-10-15 18:31:03 -0400

    walleye added to vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 #aosp_devices=('blueline' 'bonito' 'crosshatch' 'dragon' 'marlin' 'sargo' 'taimen')
 #caf_devices=('oneplus3')
-aosp_devices=('taimen')
+#aosp_devices=('taimen')
+aosp_devices=('walleye')
 
 function lunch_devices() {
     add_lunch_combo abc_${device}-user
